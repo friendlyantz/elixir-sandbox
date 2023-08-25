2d7dd36fbfaf32eaf4783f81432774b5a74260db
friendlyantz <friendlyantz@pm.me>
2023-08-25 23:28:53 +1000

    INIT Pacman dir
    mix new pacman

---
+++ pacman/lib/pacman.ex
@@ -0,0 +1,18 @@
+defmodule Pacman do
+  @moduledoc """
+  Documentation for `Pacman`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Pacman.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
