48de46d507415a5867427326d493c9f1988a5901
friendlyantz <friendlyantz@pm.me>
2023-08-26 16:33:26 +1000

    Refactor

--- phoenix_keygen_htmx/lib/phoenix_keygen_htmx.ex
+++ phoenix_keygen_htmx/lib/phoenix_keygen_htmx.ex
@@ -11,16 +11,19 @@ defmodule PhoenixKeygenHtmx do
     validate_length(contains_length?, options)
   end
 
-  defp validate_length(false, _options) do
-    # IEx.pry()
-    {:error, "no length given"}
-  end
+  defp validate_length(false, _options), do: {:error, "no length given"}
 
   defp validate_length(true, options) do
-    unless is_integer(options["length"]) do
-      {:error, "invalid type, should be Integer"}
-    else
-      {:ok, "Hey"}
-    end
+    is_length? = is_integer(options["length"])
+    validate_int_type(is_length?, options)
+  end
+
+  defp validate_int_type(false, _options), do: {:error, "invalid type, should be Integer"}
+
+  defp validate_int_type(true, _options) do
+
+    IEx.pry()
+    validate_options_type(values)
+    {:ok, "Hey"}
   end
 end
